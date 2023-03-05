f5de768da9fdca3198cbc4a1a372dba6ec9b3675
ymozer <yusufmetinozer@gmail.com>
2023-03-05 14:32:27 +0300

    reformat

--- main.cpp
+++ main.cpp
@@ -18,36 +18,25 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color);
 void drawTriangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color);
 
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
 
 	std::srand(std::time(nullptr));
 
-	if (2 == argc) {
+	if (2 == argc)
 		model = new Model(argv[1]);
-	}
-	else {
+	else 
 		model = new Model("media/african_head.obj");
-	}
 
 	TGAImage image(width, height, TGAImage::RGB);
-	for (int i = 0; i < model->nfaces(); i++) {
+	for (int i = 0; i < model->nfaces(); i++) 
+	{
 		std::vector<int> face = model->face(i);
 		std::vector<Vec2i> lines;
-		for (int j = 0; j < 3; j++) {
+		for (int j = 0; j < 3; j++) 
+		{
 			Vec3f v0 = model->vert(face[j]);
 			lines.push_back(Vec2i((v0.x + 1.0f) * width / 2., (v0.y + 1.0f) * height / 2.0f));
-			//Vec3f v1 = model->vert(face[(j + 1) % 3]);
-			//
-			//int x0 = (v0.x + 1.) * width / 2.;
-			//int y0 = (v0.y + 1.) * height / 2.;
-			//Vec2i t0{ x0,y0 };
-			//
-			//int x1 = (v1.x + 1.) * width / 2.;
-			//int y1 = (v1.y + 1.) * height / 2.;
-			//Vec2i t1{ x1,y1 };
-			//lines.push_back(t0);
-			//lines.push_back(t1);
-			//line(x0, y0, x1, y1, image, white);
 		}
 		drawTriangle(lines[0], lines[1], lines[2], image, TGAColor(std::rand() % 256, std::rand() % 256, std::rand() % 256,255));
 	}
@@ -62,69 +51,64 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color)
+void drawTriangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) 
 {
-	line(t0.x, t0.y, t1.x, t1.y, image, color);
-	line(t1.x, t1.y, t2.x, t2.y, image, color);
-	line(t2.x, t2.y, t0.x, t0.y, image, color);
-}
-
-void drawTriangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
 	if (t0.y == t1.y && t1.y == t2.y) return;
 	// sort vertices by y-coordinate
-	if (t0.y > t1.y) {
-		std::swap(t0, t1);
-	}
-	if (t1.y > t2.y) {
-		std::swap(t1, t2);
-	}
-	if (t0.y > t1.y) {
-		std::swap(t0, t1);
-	}
+	if (t0.y > t1.y) std::swap(t0, t1);
+	if (t1.y > t2.y) std::swap(t1, t2);
+	if (t0.y > t1.y) std::swap(t0, t1);
 
 	// draw bottom to top
 	const int total_height = t2.y - t0.y;
-	for (int i = 0; i < total_height; i++) {
+	for (int i = 0; i < total_height; i++) 
+	{
 		bool second_half = i > t1.y - t0.y || t1.y == t0.y;
 		int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
 		float alpha = (float)i / total_height;
-		float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height; // be careful: with above conditions no division by zero here 
+		float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height;
 		Vec2i A = t0 + (t2 - t0) * alpha;
 		Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
 		if (A.x > B.x) std::swap(A, B);
-		for (int j = A.x; j <= B.x; j++) {
-			image.set(j, t0.y + i, color); // attention, due to int casts t0.y+i != A.y 
+		for (int j = A.x; j <= B.x; j++) 
+		{
+			image.set(j, t0.y + i, color);
 		}
 	}
 }
 
 
 // error * dx * 2 assumed error 
-void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) 
+{
 	bool steep = false;
-	if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
+	if (std::abs(x0 - x1) < std::abs(y0 - y1)) 
+	{
 		std::swap(x0, y0);
 		std::swap(x1, y1);
 		steep = true;
 	}
-	if (x0 > x1) {
+
+	if (x0 > x1) 
+	{
 		std::swap(x0, x1);
 		std::swap(y0, y1);
 	}
+
 	int dx = x1 - x0;
 	int dy = y1 - y0;
 	int derror2 = std::abs(dy) * 2;
 	int error2 = 0;
 	int y = y0;
-	for (int x = x0; x <= x1; x++) {
-		if (steep) {
-			image.set(y, x, color);
-		}
-		else {
-			image.set(x, y, color);
-		}
+
+	for (int x = x0; x <= x1; x++) 
+	{
+		if (steep) image.set(y, x, color);
+		else image.set(x, y, color);
+
 		error2 += derror2;
-		if (error2 > dx) {
+		if (error2 > dx) 
+		{
 			y += (y1 > y0 ? 1 : -1);
 			error2 -= dx * 2;
 		}
